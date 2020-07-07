c06063308fc28463ad986f01265623fecb96b1b5
leipan <leipan@xueqiu.com>
2020-07-07 17:20:58 +0800

    feat: 提供解析 body 参数的 npm 包 koa-body

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
 
-app.use(async (ctx, next) => {
-	let stime = new Date().getTime();
-	await next();
-	let etime = new Date().getTime();
-	// 移交控制权
-	ctx.response.type = 'text/html';
-	ctx.response.body = '<h1>Hello World</h1>';
-	console.log(`请求地址: ${ctx.path}，响应时间：${etime - stime}ms`)
+app.use(bodyParser())
+
+router.get('/', async(ctx, next) => {
+  ctx.response.body = `<h1>index page</h1>`
 })
 
-app.use(async (ctx, next)=>{
-	console.log('中间件 1 doSomething');
-	await next();
-	console.log('中间件 1 end')
+router.get('/home', async(ctx, next) => {
+  console.log(ctx.request.query)
+  console.log(ctx.request.querystring)
+  ctx.response.body = '<h1>HOME page</h1>'
 })
 
-app.listen(3000, ()=>{
-	console.log('server is running at http://localhost:3000');
+router.get('/home/:id/:name', async(ctx, next)=>{
+  console.log(ctx.params)
+  ctx.response.body = '<h1>HOME page /:id/:name</h1>'
+})
+
+router.get('/404', async(ctx, next) => {
+  ctx.response.body = '<h1>404 Not Found</h1>'
+})
+
+ // 增加返回表单页面的路由
+router.get('/user', async(ctx, next)=>{
+	ctx.response.body = 
+	`
+	<form action="/user/register" method="post">
+		<input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
+		<br/>
+		<input name="password" type="text" placeholder="请输入密码：123456"/>
+		<br/> 
+		<button>GoGoGo</button>
+	</form>
+	`
+})
+
+   // 增加响应表单请求的路由
+router.post('/user/register',async(ctx, next)=>{
+	let {name, password} = ctx.request.body
+	if( name === 'ikcamp' && password === '123456' ){
+		ctx.response.body = `Hello， ${name}！`
+	}else{
+		ctx.response.body = '账号信息错误'
+	}
+})
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
 })
